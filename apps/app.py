e8df714f0330ddbc4b8c6d410406c41a797806dc
asapeee <atakuma00@gmail.com>
2023-12-02 15:13:35 +0900

    change app

--- apps/app.py
+++ apps/app.py
@@ -6,54 +6,6 @@ from flask_wtf import CSRFProtect
 from apps.config import config
 from flask_login import LoginManager
 
-
-db = SQLAlchemy()
-csrf = CSRFProtect()
-login_manager = LoginManager()
-login_manager.login_view = 'auth.signup'
-login_manager.login_message = ''
-
-
-def create_app(config_key):
-    app = Flask(__name__)
-
-    app.jinja_env.filters['zip'] = zip
-
-    app.config.from_object(config[config_key])
-
-    # app.config.from_mapping(
-    #     SECRET_KEY="2AZSMss3p5QPbcY2hBsJ",
-    #     SQLALCHEMY_DATABASE_URI=f"sqlite:///{Path(__file__).parent.parent / 'local.sqlite'}",
-    #     SQLALCHEMY_TRACK_MODIFICATIONS=False,
-    #     SQLALCHEMY_ECHO=True,
-    #     WTF_CSRF_SECRET_KEY="AuwzyszU5sugKN7KZs6f",
-    # )
-
-    csrf.init_app(app)
-    db.init_app(app)
-
-    Migrate(app, db)
-
-    login_manager.init_app(app)
-
-    from apps.crud import views as crud_views
-
-    app.register_blueprint(crud_views.crud, url_prefix='/crud')
-
-    from apps.auth import views as auth_views
-
-    app.register_blueprint(auth_views.auth, url_prefix='/auth')
-
-    from apps.recorder import views as rc_views
-
-    app.register_blueprint(rc_views.rc)
-
-    app.register_error_handler(404, page_not_found)
-    app.register_error_handler(500, internal_server_error)
-
-    return app
-
-
 def page_not_found(e):
     return render_template('404.html'), 404
 
@@ -61,3 +13,47 @@ def page_not_found(e):
 def internal_server_error(e):
     return render_template('500.html'), 500
 
+
+db = SQLAlchemy()
+csrf = CSRFProtect()
+login_manager = LoginManager()
+login_manager.login_view = 'auth.signup'
+login_manager.login_message = ''
+
+config_key = "local"
+
+app = Flask(__name__)
+
+app.jinja_env.filters['zip'] = zip
+
+app.config.from_object(config[config_key])
+
+# app.config.from_mapping(
+#     SECRET_KEY="2AZSMss3p5QPbcY2hBsJ",
+#     SQLALCHEMY_DATABASE_URI=f"sqlite:///{Path(__file__).parent.parent / 'local.sqlite'}",
+#     SQLALCHEMY_TRACK_MODIFICATIONS=False,
+#     SQLALCHEMY_ECHO=True,
+#     WTF_CSRF_SECRET_KEY="AuwzyszU5sugKN7KZs6f",
+# )
+
+csrf.init_app(app)
+db.init_app(app)
+
+Migrate(app, db)
+
+login_manager.init_app(app)
+
+from apps.crud import views as crud_views
+
+app.register_blueprint(crud_views.crud, url_prefix='/crud')
+
+from apps.auth import views as auth_views
+
+app.register_blueprint(auth_views.auth, url_prefix='/auth')
+
+from apps.recorder import views as rc_views
+
+app.register_blueprint(rc_views.rc)
+
+app.register_error_handler(404, page_not_found)
+app.register_error_handler(500, internal_server_error)
