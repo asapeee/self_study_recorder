05d25ab69a155e219eeb5bf1a76e0eba3e3d27d6
asapeee <atakuma00@gmail.com>
2023-12-06 22:26:26 +0900

    student_idをstring型からintger型に変更

--- apps/recorder/models.py
+++ apps/recorder/models.py
@@ -6,7 +6,7 @@ from apps.app import db
 class StudentRecord(db.Model):
     __tablename__ = 'student_records'
     id = db.Column(db.Integer, primary_key=True)
-    student_id = db.Column(db.String, db.ForeignKey('students.id'))
+    student_id = db.Column(db.Integer, db.ForeignKey('students.id'))
     studentname = db.Column(db.String)
     started_at = db.Column(db.DateTime, nullable=True)
     finished_at = db.Column(db.DateTime, nullable=True)
@@ -16,7 +16,7 @@ class StudentRecord(db.Model):
 class StudentMonthRecord(db.Model):
     __tablename__ = 'student_month_records'
     id = db.Column(db.Integer, primary_key=True)
-    student_id = db.Column(db.String, db.ForeignKey('students.id'))
+    student_id = db.Column(db.Integer, db.ForeignKey('students.id'))
     studentname = db.Column(db.String)
     year_month = db.Column(db.String)
     total_time = db.Column(db.Float, default=0)
